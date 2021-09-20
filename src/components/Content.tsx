f738d2dfe66cd02359ddbdd8c5337b79e7c4d05f
allissonneo <allissonneo@gmail.com>
2021-09-20 12:42:40 -0300

    componentização do App.tsx componentes envolvidos: Content.tsx SideBar.tsx

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,43 @@
-export function Content() {
-  // Complete aqui
+import { JSXElementConstructor, ReactElement, ReactNodeArray, ReactPortal, useEffect, useState } from "react";
+import { api } from "../services/api";
+import { MovieCard } from "./MovieCard";
+import '../styles/global.scss';
+import '../styles/content.scss'
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: { generoSelecionadoId: number; generoSelecionado: string; }) {
+  const [movies, setMovies] = useState<MovieProps[]>([]);
+
+  useEffect(() => {
+    api.get<MovieProps[]>(`movies/?Genre_id=${props.generoSelecionadoId}`).then(response => {
+      setMovies(response.data);
+  })}, [props.generoSelecionadoId])
+
+
+
+  return (
+    <div className="container">
+      <header>
+        <span className="category">Categoria:<span> {props.generoSelecionado}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
 }
